2cca4f72d90a384b32af9b7f0cd86f37dcd86c91
rohanthewiz@gmail.com <rohanthewiz@gmail.com>
2019-01-21 14:52:58 -0600

    Rework for using TLS via generated certs

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/rohanthewiz/rxrun
 require (
 	github.com/mholt/certmagic v0.0.0-20190118173747-01ffe8b3c7d6
 	github.com/miekg/dns v1.1.3 // indirect
+	github.com/rohanthewiz/element v0.1.2
 	github.com/rohanthewiz/rxrouter v0.0.0-20190121051559-e829aa3909d5
 	github.com/valyala/fasthttp v1.1.0
 	github.com/xenolf/lego v2.0.1+incompatible // indirect
--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/rohanthewiz/element"
 	"github.com/rohanthewiz/rxrouter"
 	"github.com/valyala/fasthttp"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	rx := rxrouter.New(rxrouter.Options{
 		Verbose: true,
 		TLS: rxrouter.RxTLS{
-			UseTLS:   true,
+			UseTLS:   false,
 			CertFile: "/var/lib/acme/live/www.ccswm.org/cert",
 			KeyFile:  "/var/lib/acme/live/www.ccswm.org/privkey",
 		},
@@ -21,6 +23,10 @@ func main() {
 	prt := os.Getenv("RX_PORT")
 	if prt != "" {
 		rx.Options.Port = prt
+	}
+	tls := strings.ToLower(os.Getenv("RX_USE_TLS"))
+	if tls != "" && tls != "false" && tls != "no" {
+		rx.Options.TLS.UseTLS = true
 	}
 
 	// Logging middleware
@@ -45,11 +51,10 @@ func main() {
 	)
 
 	// Add some routes
-	rx.AddRoute("/", func(ctx *fasthttp.RequestCtx, params map[string]string) {
-		fmt.Fprintf(ctx, "Hello, world! Requested path is %q", ctx.Path())
+	rx.AddRoute("/", handleRoot)
+	rx.AddRoute("/hello/:name/:age", func(ctx *fasthttp.RequestCtx, params map[string]string) {
+		ctx.WriteString(fmt.Sprintf("Hello %s. You are %s!", params["name"], params["age"]))
 	})
-	rx.AddRoute("/hello/:name/:age", handleHello)
-	rx.AddRoute("/store/:number/:location", handleStore)
 	// Routes for static files
 	rx.AddStaticFilesRoute("/images/", "./assets/images", 1)
 	rx.AddStaticFilesRoute("/css/", "./assets/css", 1)
@@ -59,10 +64,19 @@ func main() {
 	rx.Start()
 }
 
-func handleHello(ctx *fasthttp.RequestCtx, params map[string]string) {
-	ctx.WriteString(fmt.Sprintf("Hello %s", params["name"]))
-}
-
-func handleStore(ctx *fasthttp.RequestCtx, params map[string]string) {
-	fmt.Fprintf(ctx, "Store: %s  location: %s", params["number"], params["location"])
+func handleRoot(ctx *fasthttp.RequestCtx, params map[string]string) {
+	e := element.New
+	htm := "<!DOCTYPE html>" +
+		e("html").R(
+			e("head").R(
+				e("title").R("Under Maintenance"),
+			),
+			e("body").R(
+				e("div", "class", "main-content").R(
+					e("h2").R("This site is currently under maintenance. Please check back later, and thanks for your patience"),
+				),
+			),
+		)
+	ctx.SetContentType("text/html; charset=utf-8")
+	ctx.WriteString(htm)
 }
