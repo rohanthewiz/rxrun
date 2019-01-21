5c050fd4b8a393a1aa8354920521e36794809450
rohanthewiz@gmail.com <rohanthewiz@gmail.com>
2019-01-21 00:19:50 -0600

    Rework for using TLS via generated certs

--- go.mod
+++ go.mod
@@ -1,6 +1,14 @@
 module github.com/rohanthewiz/rxrun
 
 require (
-	github.com/rohanthewiz/rxrouter v0.0.0-20190120052508-ae3514738589
+	github.com/mholt/certmagic v0.0.0-20190118173747-01ffe8b3c7d6
+	github.com/miekg/dns v1.1.3 // indirect
+	github.com/rohanthewiz/element v0.1.2
+	github.com/rohanthewiz/rxrouter v0.0.0-20190127073143-ee2117daa705
 	github.com/valyala/fasthttp v1.1.0
+	github.com/xenolf/lego v2.0.1+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20190103213133-ff983b9c42bc // indirect
+	golang.org/x/sys v0.0.0-20190116161447-11f53e031339 // indirect
+	golang.org/x/text v0.3.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/rohanthewiz/element"
 	"github.com/rohanthewiz/rxrouter"
 	"github.com/valyala/fasthttp"
 	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	rx := rxrouter.New(
-		rxrouter.Options{Verbose: true, Port: "3026"}, // the Options argument here is optional
-	)
+	rx := rxrouter.New(rxrouter.Options{
+		Verbose: true,
+		TLS: rxrouter.RxTLS{
+			UseTLS:   false,
+			CertFile: "/etc/letsencrypt/live/mysite.com/cert.pem",
+			KeyFile:  "/etc/letsencrypt/live/mysite.com/privkey.pem",
+		},
+	})
+	// Env port override
+	prt := os.Getenv("RX_PORT")
+	if prt != "" {
+		rx.Options.Port = prt
+	}
+	tls := strings.ToLower(os.Getenv("RX_USE_TLS"))
+	if tls != "" && tls != "false" && tls != "no" {
+		rx.Options.TLS.UseTLS = true
+		go startHttpToHTTPSRedirector()
+	}
 
 	// Logging middleware
 	rx.Use(
@@ -34,23 +52,50 @@ func main() {
 	)
 
 	// Add some routes
-	rx.AddRoute("/", func(ctx *fasthttp.RequestCtx, params map[string]string) {
-		fmt.Fprintf(ctx, "Hello, world! Requested path is %q", ctx.Path())
+	rx.AddRoute("/", handleRoot)
+	rx.AddRoute("/hello/:name/:age", func(ctx *fasthttp.RequestCtx, params map[string]string) {
+		ctx.WriteString(fmt.Sprintf("Hello %s. You are %s!", params["name"], params["age"]))
 	})
-	rx.AddRoute("/hello/:name/:age", handleHello)
-	rx.AddRoute("/store/:number/:location", handleStore)
 	// Routes for static files
 	rx.AddStaticFilesRoute("/images/", "./assets/images", 1)
 	rx.AddStaticFilesRoute("/css/", "./assets/css", 1)
+	rx.AddStaticFilesRoute("/.well-known/acme-challenge/", "./certs", 0)
 
 	// Let it rip!
 	rx.Start()
 }
 
-func handleHello(ctx *fasthttp.RequestCtx, params map[string]string) {
-	ctx.WriteString(fmt.Sprintf("Hello %s", params["name"]))
+func handleRoot(ctx *fasthttp.RequestCtx, params map[string]string) {
+	e := element.New
+	htm := "<!DOCTYPE html>" +
+		e("html").R(
+			e("head").R(
+				e("title").R("Under Maintenance"),
+			),
+			e("body").R(
+				e("div", "class", "main-content").R(
+					e("h2").R("This site is currently under maintenance. Please check back later, and thanks for your patience"),
+				),
+			),
+		)
+	ctx.SetContentType("text/html; charset=utf-8")
+	ctx.WriteString(htm)
 }
 
-func handleStore(ctx *fasthttp.RequestCtx, params map[string]string) {
-	fmt.Fprintf(ctx, "Store: %s  location: %s", params["number"], params["location"])
+// Use a simple router instance to redirect traffic from standard to TLS port
+// Typically this is 80 -> 443
+func startHttpToHTTPSRedirector() {
+	fx := rxrouter.New(rxrouter.Options{
+		Verbose: true,
+		Port:    "80",
+		TLS: rxrouter.RxTLS{
+			UseTLS: false, // just being explicit here
+		},
+	})
+
+	fx.AddCustomMasterHandler(func(ctx *fasthttp.RequestCtx) {
+		ctx.Redirect("https://mysite.com", fasthttp.StatusTemporaryRedirect)
+	})
+
+	fx.Start()
 }
